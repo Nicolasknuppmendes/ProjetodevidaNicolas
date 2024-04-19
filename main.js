01d34ab070bd8dbd5c591493b996573a26c93d25
Nicolasknuppmendes <104834111+Nicolasknuppmendes@users.noreply.github.com>
2024-04-19 13:31:07 -0300

    .

--- main.js
+++ main.js
@@ -28,11 +28,16 @@ let tempoAtual = new Date();
 
 contadores[0].textContent = calculaTempo(tempos[0]);
 
-function atualizaCronometro(){
+function atualizaCronometro(){ 
+    document.getElementById("dias0").textContent = calculaTempo(tempos[0])[0];
+    document.getElementById("horas0").textContent = calculaTempo(tempos[0])[1];  
+    document.getElementById("min0").textContent = calculaTempo(tempos[0])[2];
+    document.getElementById("seg0").textContent = calculaTempo(tempos[0])[3];
     for (let i=0; i<contadores.length;i++){
-        contadores[i].textContent = calculaTempo(tempos[i]);   
+        //contadores[i].textContent = calculaTempo(tempos[i]);   
     }
 }
+
 function comecaCronometro(){
     atualizaCronometro();
     setInterval(atualizaCronometro,1000);
